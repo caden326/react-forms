71edb127be288181020a3488bf56f145a6e85faa
caden326 <84342510+caden326@users.noreply.github.com>
2021-07-09 13:43:25 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import UserForm from './components/registration';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <UserForm/>
+
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
